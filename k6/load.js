3057a4b53e888c65ab3eb2b043ba26403346cbae
jhonny robert <jhonnyrobert.s@outlook.com>
2023-04-25 15:09:49 -0300

    v1/feature/Adicionado thresholds dos requisitos de aceite do desafio

--- k6/load.js
+++ k6/load.js
@@ -3,12 +3,16 @@ import { sleep } from 'k6';
 
 export const options = {
   vus: 10,
-  duration: '06s',
+  duration: '30s',
+  thresholds: {
+    // 80% das requisições devem obter sucesso
+    'http_req_failed{scenario:default}': ['rate<0.2'],
+    // 95% das requisições devem ser menor que 5s
+    'http_req_duration{scenario:default}': ['p(95)<5000'],
+  },
 };
 
 export default function () {
   http.get('http://localhost:3000/produtos');
   sleep(1);
-
-  //docker run -v $(pwd):/k6 -i --network="host" loadimpact/k6 run /k6/load.js
 }
